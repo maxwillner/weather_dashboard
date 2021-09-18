1493d963b56816c9b374cdfe548156fc096f6421
Frankenstein <88062514+maxwillner@users.noreply.github.com>
2021-09-18 16:10:37 -0400

    added dynamic html elements for today forecast; styling edits

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ var citySearchInputEl = document.querySelector("#city-search")
 var searchedLocationEl = document.querySelector("#searched-location");
 var locationContainerEl = document.querySelector("#location-container");
 
+var currentDate = moment().format('l')
+
 var formSubmitHandler = function(event) {
     event.preventDefault();
 
@@ -45,9 +47,36 @@ var getLocationWeather = function(location) {
             return response.json();
     })
         .then(function(response) {
-            console.log(response.temp);
-        });
+            // pass response into dom function
 
+            // current day weather
+            var todayDate = document.createElement("h4");
+            todayDate.textContent = "Date: " + currentDate;
+            locationContainerEl.appendChild(todayDate);
+
+            var todayCondition = document.createElement("h4");
+            todayCondition.textContent = "Condition: " + response.daily[0].weather[0].description
+            locationContainerEl.appendChild(todayCondition);
+
+            var todayTemp = document.createElement("h4"); 
+            todayTemp.textContent = "Temperature: " + response.daily[0].temp.day
+            locationContainerEl.appendChild(todayTemp);
+
+            var todayWind = document.createElement("h4");
+            todayWind.textContent = "Wind: " + response.daily[0].wind_speed + " MPH";
+            locationContainerEl.appendChild(todayWind);
+
+            var todayHumidity = document.createElement("h4");
+            todayHumidity.textContent = "Humidity: " + response.daily[0].humidity + "%";
+            locationContainerEl.appendChild(todayHumidity);
+
+            var todayUVIndex = document.createElement("h4");
+            todayUVIndex.textContent = "UV Index: " + response.daily[0].uvi;
+            locationContainerEl.appendChild(todayUVIndex);
+
+            // 5 day forecast
+
+        });
 }
 
 var displayLocation = function (location) {
